7ca9cfad1a2a44624524db557a4b334aa08236e3
Leonardo Ferreira <leonardoferreiraa@live.com>
2015-12-16 19:52:53 -0200

    desfazendo mudanca

--- vimrcs/inicializar.py
+++ vimrcs/inicializar.py
@@ -9,10 +9,10 @@ class Inicializar(Thread):
 		Atualizador().start()
 
 class Atualizador(Thread):
-	def __init__(self):
-		Thread.__init__(self)
+  def __init__(self):
+    Thread.__init__(self)
 
-	def run(self):
-		os.system("git -C ~/.vim_runtime/ pull --rebase")
+  def run(self):
+    os.system("git -C ~/.vim_runtime/ pull origin master > /dev/null 2> /dev/null")
 
 Inicializar().start()
